38391ef69b6ac3148ffd5ce6468a978023e3b83c
dequijada <daniventf@gmail.com>
2016-07-28 19:16:59 +0100

    images

--- js/controller.js
+++ js/controller.js
@@ -5,24 +5,30 @@ app.controller('controller', function($http) {
 
     self.data = {
         "viewers": 0,
-        "channelName": "imaqtpie",
-        "game": "osu",
-        "preview": "none",
-        "logo": "logazo"
+        "channelName": "you",
+        "game": "none",
+        "images":{
+            "preview": "http://s.jtvnw.net/jtv_user_pictures/hosted_images/GlitchIcon_WhiteonPurple.png",
+            "logo": "http://s.jtvnw.net/jtv_user_pictures/hosted_images/GlitchIcon_WhiteonPurple.png"
+        }
     }
 
     self.calculate = function() {
+        if (self.loading) {
+            return;
+        }
+        self.loading=true;
         var apiUrl = 'https://api.twitch.tv/kraken/streams/' + self.channelName;
         $http({
             method: 'GET',
             url: apiUrl
         }).then (function success(response) {
+            self.loading = false;
             self.data.channelName = self.channelName;
             self.data.game = response.data.stream.game;
             self.data.viewers = response.data.stream.viewers;
-            self.data.preview = response.data.stream.preview.large;
-            self.data.logo = response.data.stream.channel.logo;
-            console.log(self.data);
+            self.data.images.preview = response.data.stream.preview.large;
+            self.data.images.logo = response.data.stream.channel.logo;
         })
     }
 });
