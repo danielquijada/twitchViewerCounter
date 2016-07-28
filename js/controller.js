da49532213c5eb81a2b0069b562e84d52400d300
dequijada <daniventf@gmail.com>
2016-07-28 18:11:37 +0100

    allthepipoltesting

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,22 @@
 var app = angular.module('counter', []);
 
-app.controller('controller', function() {
+app.controller('controller', function($http) {
     var self = this;
 
     self.data = {
         "viewers": 0
     }
 
-    slef.calculate = function() {
+    self.calculate = function() {
         self.data.viewers += 1;
+        $http({
+            method: 'GET',
+            url: '/someUrl'
+        }).then (function success(response) {
+            console.log("Succ, wait for it...");
+            console.log(response);
+            console.log("ess");
+        })
         console.log(self.data.viewers);
     }
 });
