17a80900db2d7ac2e1dfa30c1185434889c8ec14
dequijada <daniventf@gmail.com>
2016-08-05 20:17:55 +0100

    pointBackgroundColor

--- js/controller.js
+++ js/controller.js
@@ -112,8 +112,9 @@ app.controller('controller', function ($http, $interval) {
                 {
                     label: "# de Viewers",
                     fillColor: "rgba(172,194,132,0.4)",
-                    strokeColor: "rgba(150,150,150,0.7)",
-                    pointColor: "rgba(75,75,75,0.7)",
+                    borderColor: "rgba(150,150,150,0.7)",
+                    pointBorderColor: "rgba(150,150,150,0.7)",
+                    pointBackgroundColor: "rgba(75,75,75,0.7)",
                     pointStrokeColor: "#9DB86D",
                     data: viewers,
                     lineTension: 0
@@ -121,8 +122,9 @@ app.controller('controller', function ($http, $interval) {
                 {
                     label: "Media",
                     fill: false,
-                    strokeColor: "rgba(0,0,0,1)",
-                    pointColor: "rgba(30,30,30,1)",
+                    borderColor: "rgba(0,0,0,1)",
+                    pointBorderColor: "rgba(0,0,0,1)",
+                    pointBackgroundColor: "rgba(30,30,30,1)",
                     data: JSON.parse(JSON.stringify(viewers)),
                     lineTension: 0
                 }
